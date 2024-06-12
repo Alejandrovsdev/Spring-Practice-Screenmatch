cba8ae87bd3e51e188f3b313505d1720547c1789
Alejandro suarez <Alejandrovs.fsdev@gmail.com>
2024-06-12 10:56:13 -0300

    se agrego la clase principal para las consultas a la api

---
+++ src/main/java/com/aluracursos/springscreenmatch/Principal.java
@@ -0,0 +1,59 @@
+package com.aluracursos.springscreenmatch;
+
+import com.aluracursos.springscreenmatch.service.ConversorDatos;
+import com.aluracursos.springscreenmatch.model.DatosEpisodio;
+import com.aluracursos.springscreenmatch.model.DatosSerie;
+import com.aluracursos.springscreenmatch.model.DatosTemporada;
+import com.aluracursos.springscreenmatch.service.ConsumoApi;
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Principal {
+    private Scanner teclado = new Scanner(System.in);
+    private final String URL = "https://www.omdbapi.com/?t=";
+    private final String APIKEY = "&apikey=c924f56b";
+    private ConsumoApi consumoAPI = new ConsumoApi();
+    private ConversorDatos conversor = new ConversorDatos();
+    public void muestraElMenu() throws JsonProcessingException {
+        System.out.println("Escribe el nombre de la série que deseas buscar");
+        var nombreSerie = teclado.nextLine();
+
+        var json = consumoAPI.obtenerDatos(URL + nombreSerie.replace(" ", "+") + APIKEY);
+        DatosSerie datos = conversor.obtenerDatos(json, DatosSerie.class);
+
+        List<DatosTemporada> temporadas = new ArrayList<>();
+
+        for (int i = 1; i <= datos.totalTemporadas(); i++) {
+            json = consumoAPI.obtenerDatos(URL + nombreSerie.replace(" ", "+") + "&Season=" + i + APIKEY);
+            DatosTemporada datosTemporada = conversor.obtenerDatos(json, DatosTemporada.class);
+            temporadas.add(datosTemporada);
+        }
+        //temporadas.forEach(System.out::println);
+
+       /* for (int i = 0; i < datos.totalTemporadas(); i++) {
+            List<DatosEpisodio> episodiosTemporadas = temporadas.get(i).episodios();
+            for (int j = 0; j < episodiosTemporadas.size(); j++) {
+                System.out.println(episodiosTemporadas.get(j).titulo());
+            }
+        }*/ //imprime todos los episodios de todas las temporadas de la serie
+
+        //temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo()))); // usa lambda para imprimir todos los episodios de todas las temporadas de la serie
+
+        //temporadas.get(1).episodios().forEach(e -> System.out.println(e.titulo())); //imprime todos los episodios de la primera temporada (1)
+
+        List<DatosEpisodio> datosEpisodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream())
+                .collect(Collectors.toList()); //datos de cada episodio con stream y lambdas
+
+        datosEpisodios.stream()
+                .sorted(Comparator.comparing(DatosEpisodio::evaluacion).reversed()) //compara los episodios por evaluacion y los ordena de mayor a menor
+                .filter(e -> !e.evaluacion().equalsIgnoreCase("N/A")) //filtra para evitar los episodios que en evaluacion muestran N/A
+                .limit(5) // limita a 5 episodios, es decir top 5.
+                .forEach(e -> System.out.println(e.titulo() + " = " + e.evaluacion())); //imprime tituloEpisodio = evaluacion
+    }
+}
--- src/main/java/com/aluracursos/springscreenmatch/SpringScreenmatchApplication.java
+++ src/main/java/com/aluracursos/springscreenmatch/SpringScreenmatchApplication.java
@@ -1,17 +1,13 @@
 package com.aluracursos.springscreenmatch;
 
-import com.aluracursos.springscreenmatch.model.DatosPelicula;
-import com.aluracursos.springscreenmatch.model.DatosSerie;
-import com.aluracursos.springscreenmatch.service.ConsumoApi;
-import com.aluracursos.springscreenmatch.service.ConversorDatos;
+
+import com.aluracursos.springscreenmatch.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Scanner;
-
 @SpringBootApplication
-public class SpringScreenmatchApplication implements CommandLineRunner {
+public class SpringScreenmatchApplication  implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringScreenmatchApplication.class, args);
@@ -19,31 +15,9 @@ public class SpringScreenmatchApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Scanner scan = new Scanner(System.in);
-        var consumoApi = new ConsumoApi();
+        Principal principal = new Principal();
+        principal.muestraElMenu();
 
-        // Pide el título de una pelicula o serie al usuario y le da formato para poder ser introducida a la Url
-        System.out.println("Ingrese el titulo de una pelicula o serie");
-        String titulo = scan.nextLine();
-        titulo = titulo.replace(" ", "+");
-        String enlace = "http://www.omdbapi.com/?t="+titulo+"&apikey=c924f56b";
-
-        // Obtiene el json con los datos de la serie y los deserializa a una clase sea DatosSerie o DatosPelicula
-        var json = consumoApi.obtenerDatos(enlace);
-        ConversorDatos conversor = new ConversorDatos();
-
-        // Verificar el tipo antes de deserializar
-        String tipo = conversor.obtenerTipo(json);
-
-        // Verifica el tipo de contenido y obtiene los datos de la serie o la película
-        if ("series".equalsIgnoreCase(tipo)) {
-            var datoSerie = conversor.obtenerDatos(json, DatosSerie.class);
-            System.out.println(datoSerie.titulo() + " " + datoSerie.totalTemporadas());
-        } else if ("movie".equalsIgnoreCase(tipo)) {
-            var datosPelicula = conversor.obtenerDatos(json, DatosPelicula.class);
-            System.out.println(datosPelicula.titulo() + " " + datosPelicula.evaluacion());
-        } else {
-            System.out.println("El tipo de contenido no es ni serie ni película.");
-        }
     }
 }
+
